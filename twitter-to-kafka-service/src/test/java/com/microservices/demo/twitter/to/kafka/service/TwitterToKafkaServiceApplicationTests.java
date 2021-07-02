b0e3291487d4ac1c5ee26a7445c93c05ad121efe
kishokun <kishorekunal0014@gmail.com>
2021-07-02 22:27:52 +0530

    adding twitter-to-kafka-service

---
+++ twitter-to-kafka-service/src/test/java/com/microservices/demo/twitter/to/kafka/service/TwitterToKafkaServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.microservices.demo.twitter.to.kafka.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootTest
+@ComponentScan(basePackages = "com.microservices.demo")
+public class TwitterToKafkaServiceApplicationTests {
+
+    @Test
+    public void contextLoad() {
+
+    }
+}
+
